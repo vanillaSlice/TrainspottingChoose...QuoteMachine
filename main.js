f84f9b106db4f109d40670159f59757aacfb5c5e
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-12-06 21:23:49 +0000

    Tidy up

--- main.js
+++ main.js
@@ -2,34 +2,34 @@ $(document).ready(function () {
 
   'use strict';
 
-  var character = 'Mark "Rent-boy" Renton',
-    film = 'Trainspotting',
-    quotes = [
-      'Choose Life.',
-      'Choose a job.',
-      'Choose a career.',
-      'Choose a family.',
-      'Choose a f**king big television.',
-      'Choose washing machines.',
-      'Choose cars.',
-      'Choose compact disc players.',
-      'Choose electrical tin openers.',
-      'Choose good health.',
-      'Choose low cholesterol.',
-      'Choose dental insurance.',
-      'Choose fixed interest mortgage repayments.',
-      'Choose a starter home.',
-      'Choose your friends.',
-      'Choose leisurewear and matching luggage.',
-      'Choose a three-piece suit on hire purchase in a range of f**king fabrics.',
-      'Choose DIY and wondering who the f**k you are on Sunday morning.',
-      'Choose sitting on that couch watching mind-numbing, spirit-crushing game shows, stuffing f**king junk food into your mouth.',
-      'Choose rotting away at the end of it all, p**sing your last in a miserable home, nothing more than an embarrassment to the selfish, f**ked up brats you spawned to replace yourselves.',
-      'Choose your future.'
-    ],
-    quoteParams = {
-      id: '#quote-text'
-    };
+  var character = 'Mark "Rent-boy" Renton';
+  var film = 'Trainspotting';
+  var quotes = [
+    'Choose Life.',
+    'Choose a job.',
+    'Choose a career.',
+    'Choose a family.',
+    'Choose a f**king big television.',
+    'Choose washing machines.',
+    'Choose cars.',
+    'Choose compact disc players.',
+    'Choose electrical tin openers.',
+    'Choose good health.',
+    'Choose low cholesterol.',
+    'Choose dental insurance.',
+    'Choose fixed interest mortgage repayments.',
+    'Choose a starter home.',
+    'Choose your friends.',
+    'Choose leisurewear and matching luggage.',
+    'Choose a three-piece suit on hire purchase in a range of f**king fabrics.',
+    'Choose DIY and wondering who the f**k you are on Sunday morning.',
+    'Choose sitting on that couch watching mind-numbing, spirit-crushing game shows, stuffing f**king junk food into your mouth.',
+    'Choose rotting away at the end of it all, p**sing your last in a miserable home, nothing more than an embarrassment to the selfish, f**ked up brats you spawned to replace yourselves.',
+    'Choose your future.'
+  ];
+  var quoteParams = {
+    id: '.quote__text'
+  };
 
   function typeText(params) {
     if (params.currentIndex <= params.text.length) {
@@ -51,7 +51,7 @@ $(document).ready(function () {
 
   function typeCharacter() {
     typeText({
-      id: '#character',
+      id: '.quote__character',
       text: character,
       currentIndex: 0
     });
@@ -59,7 +59,7 @@ $(document).ready(function () {
 
   function typeFilm() {
     typeText({
-      id: '#film',
+      id: '.quote__film',
       text: film,
       currentIndex: 0
     });
@@ -69,11 +69,13 @@ $(document).ready(function () {
   typeCharacter();
   typeFilm();
 
-  $('#next-button').click(function () {
-    typeQuote();
-  });
+  /*
+   * Add button click events.
+   */
 
-  $('#tweet-button').click(function () {
+  $('.quote__next-btn').click(typeQuote);
+
+  $('.quote__tweet-btn').click(function () {
     window.open('https://twitter.com/intent/tweet?text="' + encodeURIComponent(quoteParams.text) + '"&hashtags=trainspotting');
   });
 
